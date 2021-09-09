a98076c9804c1d49104a765d23021bafeb8b3c31
Santiago Santana <ssantana@narrativescience.com>
2021-09-09 12:40:59 -0500

    test issues

--- tests/functional/test_database_model.py
+++ tests/functional/test_database_model.py
@@ -1,8 +1,8 @@
 """Tests for DatabaseModel abstract class"""
-import os
 import asyncio
 from asyncio import gather, sleep
 from datetime import datetime
+import os
 from typing import Optional
 from uuid import uuid4
 
@@ -17,16 +17,20 @@ from pynocular.exceptions import DatabaseModelMissingField, DatabaseRecordNotFou
 
 db_user_password = str(os.environ.get("DB_USER_PASSWORD"))
 # DB to initially connect to so we can create a new db
-existing_connection_string = str(os.environ.get(
-    "EXISTING_DB_CONNECTION_STRING",
-    f"postgresql://postgres:{db_user_password}@localhost:5432/postgres?sslmode=disable",
-))
+existing_connection_string = str(
+    os.environ.get(
+        "EXISTING_DB_CONNECTION_STRING",
+        f"postgresql://postgres:{db_user_password}@localhost:5432/postgres?sslmode=disable",
+    )
+)
 
 test_db_name = str(os.environ.get("TEST_DB_NAME", "test_db"))
-test_connection_string = str(os.environ.get(
-    "TEST_DB_CONNECTION_STRING",
-    f"postgresql://postgres:{db_user_password}@localhost:5432/{test_db_name}?sslmode=disable",
-))
+test_connection_string = str(
+    os.environ.get(
+        "TEST_DB_CONNECTION_STRING",
+        f"postgresql://postgres:{db_user_password}@localhost:5432/{test_db_name}?sslmode=disable",
+    )
+)
 testdb = DBInfo(DatabaseType.aiopg_engine, test_connection_string)
 
 
