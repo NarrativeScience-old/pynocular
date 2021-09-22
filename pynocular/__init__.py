e9457e9e2caf336a5ca13f2631f15400df01bc9d
Jonathan Drake <jdrake@narrativescience.com>
2021-09-22 08:21:27 -0700

    Unpin backoff version (#5)
    * BUnpin backoff version
    
    * Version bumped to 0.6.0
    
    Co-authored-by: ns-circle-ci <devops-team+circleci@narrativescience.com>

--- pynocular/__init__.py
+++ pynocular/__init__.py
@@ -1,5 +1,5 @@
 """Lightweight ORM that lets you query your database using Pydantic models and asyncio"""
 
-__version__ = "0.5.0"
+__version__ = "0.6.0"
 
 from pynocular.engines import DatabaseType, DBInfo
