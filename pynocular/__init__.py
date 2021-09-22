d046139c4d294660f0f3552bf50579c1c5e277a9
Jonathan Drake <jdrake@narrativescience.com>
2021-09-22 10:29:59 -0700

    Upgrade aiopg to 1.3.1 (#8)
    * Upgrade aiopg to 1.3.1
    
    * Version bumped to 0.9.0
    
    Co-authored-by: ns-circle-ci <devops-team+circleci@narrativescience.com>

--- pynocular/__init__.py
+++ pynocular/__init__.py
@@ -1,5 +1,5 @@
 """Lightweight ORM that lets you query your database using Pydantic models and asyncio"""
 
-__version__ = "0.8.0"
+__version__ = "0.9.0"
 
 from pynocular.engines import DatabaseType, DBInfo
