6d25e731d02895635b6c883334555a5356142d8b
ns-circle-ci <devops-team+circleci@narrativescience.com>
2021-10-19 22:23:41 +0000

    Version bumped to 0.15.0

--- pynocular/__init__.py
+++ pynocular/__init__.py
@@ -1,5 +1,5 @@
 """Lightweight ORM that lets you query your database using Pydantic models and asyncio"""
 
-__version__ = "0.14.0"
+__version__ = "0.15.0"
 
 from pynocular.engines import DatabaseType, DBInfo
