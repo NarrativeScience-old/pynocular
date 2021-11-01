8dba2765004d219f8f2e5e46de50265a63334c69
Cloud User <ssantana@narrativescience.com>
2021-11-01 23:20:56 +0000

    Update cruft

--- .githooks/pre-commit-scripts/common.sh
+++ .githooks/pre-commit-scripts/common.sh
@@ -2,16 +2,6 @@
 
 export REPO_ROOT="$(git rev-parse --show-toplevel)"
 
-function checkPythonVersion() {
-  local version="$(python --version 2>&1)"
-  shopt -s nocasematch
-  if [[ "$version" != *'Python 3.6.'* ]]; then
-    echo "ERROR: The Python version in your shell ($version) is not supported by the git pre-commit hook. Please activate Python 3.6"
-    exit 1
-  fi
-  shopt -u nocasematch
-}
-
 function checkAWSCLI() {
   if [[ ! $(which aws) ]]; then
     echo "Please install AWS CLI V2 following these instructions."
