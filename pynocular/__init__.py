4ced37826593f945bb73610d8c5dde10d6ee770e
RJ Santana <ssantana@narrativescience.com>
2022-04-20 18:19:56 -0600

    Fix issue where we are storing the enum name instead of value, also a… (#21)
    * Fix issue where we are storing the enum name instead of value, also added get_table helper funct
    
    * Version bumped to 0.19.0
    
    * update black
    
    Co-authored-by: Ubuntu <rj.santana@narrativescience.com>
    Co-authored-by: ns-circle-ci <devops-team+circleci@narrativescience.com>

--- pynocular/__init__.py
+++ pynocular/__init__.py
@@ -1,6 +1,6 @@
 """Lightweight ORM that lets you query your database using Pydantic models and asyncio"""
 
-__version__ = "0.18.0"
+__version__ = "0.19.0"
 
 from pynocular.database_model import DatabaseModel, UUID_STR
 from pynocular.engines import DatabaseType, DBInfo
