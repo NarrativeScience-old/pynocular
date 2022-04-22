ac5364191cc32a1f7630bc23cb73f5840a0cae1e
Jonathan Drake <jdrake@narrativescience.com>
2022-04-22 09:41:37 -0700

    memory server defaults

--- pynocular/backends/memory.py
+++ pynocular/backends/memory.py
@@ -34,6 +34,70 @@ class MemoryDatabaseModelBackend(DatabaseModelBackend):
         # Serial primary key generator
         self._pk_generator = itertools.count(start=1)
 
+    def _set_primary_key_values(
+        self,
+        config: DatabaseModelConfig,
+        record: Dict[str, Any],
+    ) -> Dict[str, Any]:
+        """Set default values on a record for the primary keys
+
+        Args:
+            config: DatabaseModelConfig instance that contains references to a table and
+                columns that can be used to build queries suited to the backend.
+            record: The record to update
+
+        Returns:
+            updated record
+
+        """
+        for primary_key in config.primary_keys:
+            value = (
+                next(self._pk_generator)
+                if isinstance(primary_key.type, Integer)
+                else str(uuid4())
+            )
+            record.setdefault(primary_key.name, value)
+
+        return record
+
+    @staticmethod
+    def _update_db_managed_fields(
+        config: DatabaseModelConfig,
+        record: Dict[str, Any],
+        fetch_on_create: bool = False,
+        fetch_on_update: bool = False,
+    ) -> Dict[str, Any]:
+        """Update record values for db managed fields
+
+        Args:
+            config: DatabaseModelConfig instance that contains references to a table and
+                columns that can be used to build queries suited to the backend.
+            record: The record to update
+            fetch_on_create: Flag that controls whether the db managed field will be
+                updated if it has the option `fetch_on_create=True`. Defaults to False.
+            fetch_on_update: Flag that controls whether the db managed field will be
+                updated if it has the option `fetch_on_update=True`. Defaults to False.
+
+        Raises:
+            NotImplementedError: if a field sets fetch_on_create or fetch_on_update to
+                true but its type is not supported
+
+        Returns:
+            updated record
+
+        """
+        for name in config.db_managed_fields:
+            field = config.fields[name]
+            if (fetch_on_create and field.field_info.extra.get("fetch_on_create")) or (
+                fetch_on_update and field.field_info.extra.get("fetch_on_update")
+            ):
+                if field.type_ == datetime:
+                    record[name] = datetime.utcnow()
+                else:
+                    raise NotImplementedError(field.type_)
+
+        return record
+
     def transaction(self) -> Any:
         """Create a new transaction
 
@@ -110,13 +174,10 @@ class MemoryDatabaseModelBackend(DatabaseModelBackend):
 
         """
         for record in records:
-            for primary_key in config.primary_keys:
-                value = (
-                    next(self._pk_generator)
-                    if isinstance(primary_key.type, Integer)
-                    else str(uuid4())
-                )
-                record.setdefault(primary_key.name, value)
+            self._set_primary_key_values(config, record)
+            self._update_db_managed_fields(
+                config, record, fetch_on_create=True, fetch_on_update=True
+            )
 
         self.records[config.table.name].extend(records)
 
@@ -165,6 +226,7 @@ class MemoryDatabaseModelBackend(DatabaseModelBackend):
         records = await self.select(config, where_expressions=where_expressions)
         for record in records:
             record.update(values)
+            self._update_db_managed_fields(config, record, fetch_on_update=True)
 
         return records
 
@@ -199,41 +261,15 @@ class MemoryDatabaseModelBackend(DatabaseModelBackend):
             and existing_records
         ):
             # All primary keys are already set and a record was found so update
-
-            # Set default values for db managed fields
-            for name in config.db_managed_fields:
-                field = config.fields[name]
-                if field.type_ == datetime:
-                    if field.field_info.extra.get("fetch_on_update"):
-                        record[name] = datetime.utcnow()
-                else:
-                    raise NotImplementedError(field.type_)
-
+            self._update_db_managed_fields(config, record, fetch_on_update=True)
             records = await self.update_records(config, where_expressions, record)
             return records[0]
-
         else:
             # Primary keys have not been set or there were no records found, so this is
             # a new record
-
-            # Set default values for db managed fields
-            for name in config.db_managed_fields:
-                field = config.fields[name]
-                if field.type_ == datetime:
-                    if field.field_info.extra.get(
-                        "fetch_on_create"
-                    ) or field.field_info.extra.get("fetch_on_update"):
-                        record[name] = datetime.utcnow()
-                else:
-                    raise NotImplementedError(field.type_)
-
-            for primary_key in config.primary_keys:
-                value = (
-                    next(self._pk_generator)
-                    if isinstance(primary_key.type, Integer)
-                    else str(uuid4())
-                )
-                record.setdefault(primary_key.name, value)
-
+            self._set_primary_key_values(config, record)
+            self._update_db_managed_fields(
+                config, record, fetch_on_create=True, fetch_on_update=True
+            )
             self.records[config.table.name].append(record)
             return record
